301e7313f01ce11266a1c25ae4145bf94d87941a
Joel Kwiecinski <jk.novaproducciones@gmail.com>
2022-11-17 23:07:21 -0300

    Cambios.

--- WSAuto/Controllers/AutoController.cs
+++ WSAuto/Controllers/AutoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using WSAuto.Data;
@@ -28,13 +29,13 @@ namespace WSAuto.Controllers
         }
 
 
-        // GET /api/auto/modelo
-        [HttpGet("{modelo}")]
-        public dynamic Get(string modelo)
+        // GET /api/auto/id
+        [HttpGet("{id}")]
+        public ActionResult<Auto> Get(int id)
         {
-            dynamic auto = (from a in _context.Autos
-                            where a.Modelo == modelo
-                            select a);
+            Auto auto = (from a in _context.Autos
+                         where a.Id == id
+                         select a).SingleOrDefault();
             if (auto == null)
             {
                 return NotFound();
@@ -44,12 +45,99 @@ namespace WSAuto.Controllers
         }
 
 
+
+        // GET /api/auto/modelo-color/modelo-color
+        [HttpGet("{parametro}/{dato}")]
+        public dynamic Get(string parametro, string dato)
+        {
+            dynamic auto = null;
+            switch (parametro)
+            {
+                case "modelo":
+                    auto = (from a in _context.Autos
+                            where a.Modelo == dato
+                            select a);
+                    break;
+                case "color":
+                    auto = (from a in _context.Autos
+                            where a.Color == dato
+                            select a);
+                    break;
+                default:
+                    return NoContent();
+            }
+
+            if (auto == null)
+            {
+                return NotFound();
+            }
+
+            return auto;
+        }
+
+
+        // GET /api/auto/marca/modelo
+        [HttpGet("{parametro}/{marca}/{modelo}")]
+        public dynamic Get(string parametro, string marca, string modelo)
+        {
+            if (parametro != "marcamodelo")
+            {
+                return NoContent();
+            }
+
+            var autos = (from a in _context.Autos
+                         where a.Marca == marca && a.Modelo == modelo
+                         select a);
+            if (autos == null)
+            {
+                return NotFound();
+            }
+
+            return autos;
+        }
+
+
         // POST /api/auto
         [HttpPost]
         public ActionResult Post([FromBody] Auto auto)
         {
             _context.Autos.Add(auto);
             _context.SaveChanges();
+            return Ok();
+        }
+
+
+        // PUT /api/auto
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Auto auto)
+        {
+            if (auto.Id != id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(auto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+
+        // DELETE /api/auto
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var auto = (from a in _context.Autos
+                        where a.Id == id
+                        select a).SingleOrDefault();
+            if (auto == null)
+            {
+                return NotFound();
+            }
+
+            _context.Autos.Remove(auto);
+            _context.SaveChanges();
+
             return Ok();
         }
 
